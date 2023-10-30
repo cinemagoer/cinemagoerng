acdf2acdb7c32ef5b9c9ff0c952a70ffc51160c9
H. Turgut Uyar <uyar@tekir.org>
2023-10-30 23:26:46 +0300

    Fix pytest parameterized test input styles

--- tests/test_title.py
+++ tests/test_title.py
@@ -8,86 +8,94 @@ def test_title_parser_should_set_imdb_id():
     assert parsed.imdb_id == 133093
 
 
-@mark.parametrize(("imdb_id", "type_"),
-                  [(133093, model.Movie),  # The Matrix
-                   (389150, model.TVMovie),  # The Matrix Defence
-                   (109151, model.Video),  # Armitage III: Poly-Matrix
-                   (390244, model.VideoGame),  # The Matrix Online
-                   (436992, model.TVSeries),  # Doctor Who
-                   (185906, model.TVMiniSeries),  # Band of Brothers
-                   (1000252, model.TVEpisode),  # Blink
-                   (15475200, model.TVSpecial)])  # Ricky Velez: Here's Everything
+@mark.parametrize(("imdb_id", "type_"), [
+    (133093, model.Movie),  # The Matrix
+    (389150, model.TVMovie),  # The Matrix Defence
+    (109151, model.Video),  # Armitage III: Poly-Matrix
+    (390244, model.VideoGame),  # The Matrix Online
+    (436992, model.TVSeries),  # Doctor Who
+    (185906, model.TVMiniSeries),  # Band of Brothers
+    (1000252, model.TVEpisode),  # Blink
+    (15475200, model.TVSpecial),  # Ricky Velez: Here's Everything
+])
 def test_title_parser_should_instantiate_correct_type(imdb_id, type_):
     parsed = web.get_title(imdb_id=imdb_id)
     assert isinstance(parsed, type_)
 
 
-@mark.parametrize(("imdb_id", "title"),
-                  [(133093, "The Matrix"),
-                   (389150, "The Matrix Defence"),
-                   (109151, "Armitage III: Poly-Matrix"),
-                   (390244, "The Matrix Online"),
-                   (436992, "Doctor Who"),
-                   (185906, "Band of Brothers"),
-                   (1000252, "Blink"),
-                   (15475200, "Ricky Velez: Here's Everything")])
+@mark.parametrize(("imdb_id", "title"), [
+    (133093, "The Matrix"),
+    (389150, "The Matrix Defence"),
+    (109151, "Armitage III: Poly-Matrix"),
+    (390244, "The Matrix Online"),
+    (436992, "Doctor Who"),
+    (185906, "Band of Brothers"),
+    (1000252, "Blink"),
+    (15475200, "Ricky Velez: Here's Everything"),
+])
 def test_title_parser_should_set_title(imdb_id, title):
     parsed = web.get_title(imdb_id=imdb_id)
     assert parsed.title == title
 
 
-@mark.parametrize(("imdb_id", "year"),
-                  [(133093, 1999),  # The Matrix
-                   (436992, 2005),  # Doctor Who (2005-)
-                   (412142, 2004),  # House M.D. (2004-2012)
-                   (185906, 2001),  # Band of Brothers (2001-2001)
-                   (3629794, None)])  # Aslan
+@mark.parametrize(("imdb_id", "year"), [
+    (133093, 1999),  # The Matrix
+    (436992, 2005),  # Doctor Who (2005-)
+    (412142, 2004),  # House M.D. (2004-2012)
+    (185906, 2001),  # Band of Brothers (2001-2001)
+    (3629794, None),  # Aslan
+])
 def test_title_parser_should_set_year(imdb_id, year):
     parsed = web.get_title(imdb_id=imdb_id)
     assert parsed.year == year
 
 
-@mark.parametrize(("imdb_id", "end_year"),
-                  [(436992, None),  # Doctor Who (2005-) (continuing series)
-                   (412142, 2012),  # House M.D. (2004-2012) (ended series)
-                   (185906, 2001)])  # Band of Brothers (2001-2001) (ended mini-series)
+@mark.parametrize(("imdb_id", "end_year"), [
+    (436992, None),  # Doctor Who (2005-)
+    (412142, 2012),  # House M.D. (2004-2012)
+    (185906, 2001),  # Band of Brothers (2001-2001) (TV Mini-Series)
+])
 def test_title_parser_should_set_end_year(imdb_id, end_year):
     parsed = web.get_title(imdb_id=imdb_id)
     assert parsed.end_year == end_year
 
 
-@mark.parametrize(("imdb_id",),
-                  [(133093,),  # The Matrix (Movie)
-                   (389150,),  # The Matrix Defence (TV Movie)
-                   (109151,),  # Armitage III: Poly-Matrix (Video)
-                   (390244,),  # The Matrix Online (Video Game)
-                   (1000252,),  # Blink (TV Episode)
-                   (15475200,)])  # Ricky Velez: Here's Everything
+@mark.parametrize(("imdb_id",), [
+    (133093,),  # The Matrix (Movie)
+    (389150,),  # The Matrix Defence (TV Movie)
+    (109151,),  # Armitage III: Poly-Matrix (Video)
+    (390244,),  # The Matrix Online (Video Game)
+    (1000252,),  # Blink (TV Episode)
+    (15475200,),  # Ricky Velez: Here's Everything (TV Special)
+])
 def test_title_parser_should_not_set_end_year_for_other_than_series(imdb_id):
     parsed = web.get_title(imdb_id=imdb_id)
     assert not hasattr(parsed, "end_year")
 
 
-@mark.parametrize(("imdb_id", "runtime"),
-                  [(133093, 136),  # The Matrix
-                   (436992, 45),  # Doctor Who
-                   (185906, 594),  # Band of Brothers (2001-2001)
-                   (3629794, None)])  # Aslan
+@mark.parametrize(("imdb_id", "runtime"), [
+    (133093, 136),  # The Matrix (Movie)
+    (436992, 45),  # Doctor Who (TV Series)
+    (185906, 594),  # Band of Brothers (TV Mini-Series)
+    (3629794, None),  # Aslan
+])
 def test_title_parser_should_set_runtime(imdb_id, runtime):
     parsed = web.get_title(imdb_id=imdb_id)
     assert parsed.runtime == runtime
 
 
-@mark.parametrize(("imdb_id",),
-                  [(390244,)])  # The Matrix Online
+@mark.parametrize(("imdb_id",), [
+    (390244,),  # The Matrix Online
+])
 def test_title_parser_should_not_set_runtime_for_video_games(imdb_id):
     parsed = web.get_title(imdb_id=imdb_id)
     assert not hasattr(parsed, "runtime")
 
 
-@mark.parametrize(("imdb_id", "genres"),
-                  [(133093, ["Action", "Sci-Fi"]),  # The Matrix
-                   (389150, ["Documentary"])])  # The Matrix Defence
+@mark.parametrize(("imdb_id", "genres"), [
+    (133093, ["Action", "Sci-Fi"]),  # The Matrix
+    (389150, ["Documentary"]),  # The Matrix Defence
+])
 def test_title_parser_should_set_genres(imdb_id, genres):
     parsed = web.get_title(imdb_id=imdb_id)
     assert parsed.genres == genres
--- tests/test_title_taglines.py
+++ tests/test_title_taglines.py
@@ -3,15 +3,16 @@ from pytest import mark
 from cinemagoerng import web
 
 
-@mark.parametrize(("imdb_id", "n", "first", "other"),
-                  [(133093, 15, "Free your mind", "Remember there is no spoon"),  # The Matrix
-                   (109151, 1, "If humans don't want me... why'd they create me?", None),  # Armitage III: Poly-Matrix
-                   (3629794, 0, None, None)])  # Aslan
-def test_title_taglines_parser_should_set_taglines(imdb_id, n, first, other):
+@mark.parametrize(("imdb_id", "n", "taglines"), [
+    (133093, 15, ["Free your mind", "Remember there is no spoon"]),  # The Matrix
+    (109151, 1, ["If humans don't want me... why'd they create me?"]),  # Armitage III: Poly-Matrix
+    (3629794, 0, []),  # Aslan
+])
+def test_title_taglines_parser_should_set_taglines(imdb_id, n, taglines):
     parsed = web.get_title(imdb_id=imdb_id)
     parsed = web.update_title(parsed, infoset="taglines")
     assert len(parsed.taglines) == n
     if n > 0:
-        assert parsed.taglines[0] == first
-    if n > 1:
-        assert other in parsed.taglines
+        assert parsed.taglines[0] == taglines[0]
+    for tagline in taglines[1:]:
+        assert tagline in parsed.taglines
