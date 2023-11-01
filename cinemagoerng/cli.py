6296b54597217483f04100c6e8ef744ed3a6a0df
H. Turgut Uyar <uyar@tekir.org>
2023-11-02 01:35:35 +0300

    Add CLI command for getting titles by IMDb id

--- cinemagoerng/cli.py
+++ cinemagoerng/cli.py
@@ -14,12 +14,42 @@
 # along with CinemagoerNG; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
 
-from cinemagoerng import __version__
+from cinemagoerng import __version__, web
+
+
+def get_item(args: Namespace) -> None:
+    if args.type == "title":
+        item = web.get_title(args.imdb_id)
+    if args.taglines:
+        item = web.update_title(item, infoset="taglines")
+    print(f"Title: {item.title} ({item.__class__.__name__})")
+    print(f"Year: {item.year}")
+    if hasattr(item, "runtime"):
+        print(f"Runtime: {item.runtime} min")
+    print(f"Genres: {', '.join(item.genres)}")
+    if args.taglines and (len(item.taglines) > 0):
+        taglines = '\n  '.join(item.taglines)
+        print(f"Taglines:\n  {taglines}")
 
 
 def main() -> None:
     parser = ArgumentParser(description="Retrieve data from the IMDb.")
     parser.add_argument("--version", action="version", version=__version__)
-    _ = parser.parse_args()
+
+    command = parser.add_subparsers(metavar="command", dest="command")
+    command.required = True
+
+    get_parser = command.add_parser("get",
+                                    help="retrieve information about an item")
+    get_parser.add_argument("type", choices=["title"],
+                            help="type of item to retrieve")
+    get_parser.add_argument("imdb_id", type=int,
+                            help="IMDb id of item to retrieve")
+    get_parser.add_argument("--taglines", action="store_true",
+                            help="include taglines")
+    get_parser.set_defaults(func=get_item)
+
+    arguments = parser.parse_args()
+    arguments.func(arguments)
