61d8df890f9007eca891759d1c8c46423938e616
H. Turgut Uyar <uyar@tekir.org>
2024-01-01 19:26:20 +0300

    Separate model tests into own module

--- tests/test_cinemagoerng.py
+++ tests/test_cinemagoerng.py
@@ -1,43 +1,7 @@
 from importlib import metadata
 
-import pytest
-
 import cinemagoerng
-from cinemagoerng.model import Movie
 
 
 def test_installed_version_should_match_tested_version():
     assert metadata.version("cinemagoerng") == cinemagoerng.__version__
-
-
-@pytest.mark.parametrize(("imdb_id", "country_codes", "countries"), [
-    ("tt0133093", ["US", "AU"], ["United States", "Australia"]),  # The Matrix
-    ("tt0389150", ["GB"], ["United Kingdom"]),  # The Matrix Defence
-])
-def test_title_countries_property_should_return_country_names(imdb_id, country_codes, countries):
-    movie = Movie(imdb_id=imdb_id, title="The Matrix", country_codes=country_codes)
-    assert movie.countries == countries
-
-
-@pytest.mark.parametrize(("imdb_id", "title", "language_codes", "languages"), [
-    ("tt0133093", "The Matrix", ["en"], ["English"]),
-    ("tt0043338", "Ace in the Hole", ["en", "es", "la"], ["English", "Spanish", "Latin"]),
-    ("tt2971344", "Matrix: First Dream", ["zxx"], ["None"]),
-])
-def test_title_languages_property_should_return_language_names(imdb_id, title, language_codes, languages):
-    movie = Movie(imdb_id=imdb_id, title=title, language_codes=language_codes)
-    assert movie.languages == languages
-
-
-@pytest.mark.parametrize(("imdb_id", "title", "language_codes", "sort_title"), [
-    ("tt0133093", "The Matrix", ["en"], "Matrix"),
-    ("tt0133093", "The Matrix", ["de"], "The Matrix"),
-    ("tt0133093", "Die Matrix", ["en"], "Die Matrix"),
-    ("tt0133093", "Die Matrix", ["de"], "Matrix"),
-    ("tt0133093", "The Matrix", ["en", "de"], "Matrix"),
-    ("tt0133093", "The Matrix", ["de", "en"], "The Matrix"),
-    ("tt0133093", "The", ["en"], "The"),
-])
-def test_title_sort_title_property_should_strip_article(imdb_id, title, language_codes, sort_title):
-    movie = Movie(imdb_id=imdb_id, title=title, language_codes=language_codes)
-    assert movie.sort_title == sort_title
